b753863b50c1f8103baa005f4238180764391115
Yuuya Fujiwara <y.fujiwara@tohotechnos.co.jp>
2020-02-17 09:06:17 +0900

    ｔｍｐ

--- totech_route_select.py
+++ totech_route_select.py
@@ -363,7 +363,8 @@ def adds_square_mission(aLocation, aSize):
 #メッセージ受信テスト
 #デコレーターを使ってリスナーと作成する
 #@vehicle.on_message(['MISSION_ACK', 'MISSION_CLEAR_ALL', 'MISSION_COUNT', 'MISSION_CURRENT', 'MISSION_ITEM', 'MISSION_ITEM_INT', 'MISSION_ITEM_REACHED', 'MISSION_REQUEST', 'MISSION_REQUEST_INT', 'MISSION_REQUEST_LIST', 'MISSION_REQUEST_PARTIAL_LIST', 'MISSION_SET_CURRENT', 'MISSION_WRITE_PARTIAL_LIST'])
-@vehicle.on_message( 'MISSION_CLEAR_ALL' )
+print("\n@vehicle.on_message( 'HEARTBEAT' )  using orator") 
+@vehicle.on_message( 'HEARTBEAT' )
 def listener(self, name, message):
 	print( " message = ", name,  ", value =", message )
 
