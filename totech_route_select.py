4fdccb130b00dbf1642e9f4c78702f491f4630bc
Yuuya Fujiwara <y.fujiwara@tohotechnos.co.jp>
2020-02-18 11:03:05 +0900

    home_location保存しない

--- totech_route_select.py
+++ totech_route_select.py
@@ -321,9 +321,14 @@ def save_mission(mission, aFileName):
     """
     #Add file-format information
     output='QGC WPL 110\n'
+
+    # とりあえずhome_locatrionは保存しない。
+    '''
     #Add home location as 0th waypoint
     home = vehicle.home_location
     output+="%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (0,1,0,16,0,0,0,0,home.lat,home.lon,home.alt,1)
+    ''''
+
     #Add commands
     for cmd in mission:
         commandline="%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (cmd.seq,cmd.current,cmd.frame,cmd.command,cmd.param1,cmd.param2,cmd.param3,cmd.param4,cmd.x,cmd.y,cmd.z,cmd.autocontinue)
